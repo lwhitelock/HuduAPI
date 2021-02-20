69eaddffabeb1e01b5739fa1ebdc5fed6238e95c
Blake <exbm@users.noreply.github.com>
2021-02-19 16:38:36 -0800

    Update Assets.psm1
    Updated Get-HuduAssets to use Primary Serial to retrieve Asset by Serial. Added Param Collection Code to streamline Adding URL Parameters.

--- Public/Assets.psm1
+++ Public/Assets.psm1
@@ -3,19 +3,36 @@ function Get-HuduAssets {
 		[Int]$id = '',
 		[Int]$assetlayoutid = '',
 		[Int]$companyid = '',
-		[String]$name =''
+		[String]$name ='',
+		[String]$primaryserial =''
 	
 	)
+	Add-Type -AssemblyName System.Web
 	
-	#
+    	$ParamCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty) 
+	$ParamCollection.Add('page_size',1000)
+	$ParamCollection.Add('page',0)
+	
+	if ($primaryserial) {
+	
+	$Resource = 'api/v1/asset_lookup'
+	$ParamCollection.Add('primary_serial',$primaryserial)
+	
+	else {
+	
+	$Resource = '/api/v1/assets'
+	
+	}	
 	$i = 0;
-		$AllAssets = do {
-		$Assets = hudu_request -Method get -Resource "/api/v1/assets?page=$i&page_size=1000"
+	
+	$AllAssets = do {
+		$ParamCollection.page=$i 
+		$Assets = hudu_request -Method get -Resource ($Resource + "?" + $ParamCollection.ToString())
 		$i++
 		$Assets.Assets
 		Write-Host "Retrieved $($Assets.Assets.count) Assets" -ForegroundColor Yellow
-		} while ($Assets.Assets.count % 1000 -eq 0 -and $Assets.Assets.count -ne 0)
 		
+		} while ($Assets.Assets.count % 1000 -eq 0 -and $Assets.Assets.count -ne 0)
 		
 	if ($id) {
 	$AllAssets = $AllAssets | where-object {$_.id -eq $id}
