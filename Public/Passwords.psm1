9f3408aa1d4eda9f46a738ce6d44cca5d107e1aa
Blake Miller <exbm@Blakes-MacBook-Air.local>
2021-02-23 12:15:57 -0800

    Update to fix some issues with get code
    wzo.junxion.tech,165.22.165.95 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFAOwKQmx3h+t1FUeMqRWg9zPYQhwTo6wAVLtiSL/MQRViX2QhHfr0W56En764QtfX+a4Jn3A7NyfzTv7gDRlXU=

--- Public/Passwords.psm1
+++ Public/Passwords.psm1
@@ -8,28 +8,34 @@ function Get-HuduPasswords {
   Add-Type -AssemblyName System.Web
   
   $ParamCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
-  $ParamCollection.add('page',0)
-  $ParamCollection.add('page_size',1000)
+  $ParamCollection.add('page', 1)
+  $ParamCollection.add('page_size',29)
 
   if ($id) {
-      $Resource = "api/v1/asset_passwords/$id"
+      $Resource = "/api/v1/asset_passwords/$($id)"
   } else {
-      $Resource = "api/v1/asset_passwords"+
+      $Resource = "/api/v1/asset_passwords"
   }
   
-  $i = 0;
+  $i = 1;
   
   #Pull multiple "Pages" of passwords with 1000 per page (make sure we get all possible objects)
   
   $AllPasswords = do {
-  
-    $ParamCollection.page=$i
-	  $Pass = hudu_request -resource ( $Resource + "?" + $ParamCollection.ToString() ) -method "GET"
-	  $i++
-	  $Pass
-	  Write-Host "Retrieved $($pass.count) Passwords" -ForegroundColor Yellow
     
-	} while ($Pass.count % 1000 -eq 0 -and $Pass.count -ne 0)
+    $ParamCollection['page']=$i
+	  $return = hudu_request -method "GET" -Resource $( $Resource + '?' + $ParamCollection.ToString() )
+	  $i++
+    $Passwords = @()
+    if ($return.asset_passwords) {
+      $Passwords += $return.asset_passwords
+    } else {
+      $Passwords += $return.asset_password
+    }
+    $Passwords
+	  Write-Host "Retrieved $($Passwords.count) Passwords" -ForegroundColor Yellow
+    
+	} while ($Passwords.count % 29 -eq 0 -and $Passwords.count -ne 0)
   
   
   #Since these cant be done by API we pull in all possible then filter out
