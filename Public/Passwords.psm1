26d910225f9d5d06e069660a92a0dba2bb498215
Blake Miller <exbm@Blakes-MacBook-Air.local>
2021-02-24 09:00:07 -0800

    add New,Set,Delete methods for Hudu Passwords

--- Public/Passwords.psm1
+++ Public/Passwords.psm1
@@ -9,7 +9,7 @@ function Get-HuduPasswords {
   
   $ParamCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
   $ParamCollection.add('page', 1)
-  $ParamCollection.add('page_size',29)
+  $ParamCollection.add('page_size',1000)
 
   if ($id) {
       $Resource = "/api/v1/asset_passwords/$($id)"
@@ -24,18 +24,18 @@ function Get-HuduPasswords {
   $AllPasswords = do {
     
     $ParamCollection['page']=$i
-	  $return = hudu_request -method "GET" -Resource $( $Resource + '?' + $ParamCollection.ToString() )
+	  $response = hudu_request -method "GET" -Resource $( $Resource + '?' + $ParamCollection.ToString() )
 	  $i++
     $Passwords = @()
-    if ($return.asset_passwords) {
-      $Passwords += $return.asset_passwords
+    if ($response.asset_passwords) {
+      $Passwords += $response.asset_passwords
     } else {
-      $Passwords += $return.asset_password
+      $Passwords += $response.asset_password
     }
     $Passwords
 	  Write-Host "Retrieved $($Passwords.count) Passwords" -ForegroundColor Yellow
     
-	} while ($Passwords.count % 29 -eq 0 -and $Passwords.count -ne 0)
+	} while ($Passwords.count % 1000 -eq 0 -and $Passwords.count -ne 0)
   
   
   #Since these cant be done by API we pull in all possible then filter out
@@ -48,4 +48,108 @@ function Get-HuduPasswords {
   }
   
   return $AllPasswords
+}
+
+function New-HuduPassword {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [String]$name ='',
+    [Parameter(Mandatory=$true)]
+    [Int]$company_id='',
+    [String]$passwordable_type='',
+    [int]$passwordable_id='',
+    $in_portal,
+    [String]$password='',
+    [string]$otp_secret='',
+    [String]$url='',
+    [String]$username='',
+    [String]$description='',
+    [String]$password_type=''
+  )
+  
+
+  $password = @{asset_password = @{}}
+  
+  $password.asset_password.add('name',$name)
+  $password.asset_password.add('company_id',$company_id)
+  $password.asset_password.add('passwordable_type',$passwordable_type)
+  $password.asset_password.add('passwordable_id',$passwordable_id)
+  $password.asset_password.add('in_portal',$in_portal)
+  $password.asset_password.add('password',$password)
+  $password.asset_password.add('otp_secret',$otp_secret)
+  $password.asset_password.add('url',$url)
+  $password.asset_password.add('username',$username)
+  $password.asset_password.add('description',$description)
+  $password.asset_password.add('password_type',$password_type)
+  
+  $json = $password | convertto-json -Depth 10
+  
+  $response = hudu_request -Method post -Resource "/api/v1/companies/$company_id/asset_passwords" -body $json
+  
+  $response
+  
+  
+}
+    
+function Set-HuduPassword {
+  Param (
+    [Parameter(Mandatory=$true)][Int]$id ='',
+    [Parameter(Mandatory=$true)][Int]$company_id='',
+    [String]$passwordable_type='',
+    [int]$passwordable_id='',
+    [bool]$in_portal,
+    [Parameter(Mandatory=$true)][String]$password='',
+    [string]$otp_secret='',
+    [String]$url='',
+    [String]$username='',
+    [String]$description='',
+    [String]$password_type='',
+    [String]$name=''
+    
+  )
+
+  $PasswordObject = @{
+    asset_password = @{
+  
+      name = $name
+      company_id = $company_id
+      passwordable_type = $passwordable_type
+      passwordable_id = $passwordable_id
+      in_portal= $in_portal
+      password = $password
+      otp_secret = $otp_secret
+      url = $url
+      username = $username
+      description = $description
+      password_type = $password_type
+
+    }
+  }
+
+  #remove Empty Keys
+
+  ($PasswordObject.asset_password.GetEnumerator() | ? { -not $_.Value }) | % { $PasswordObject.asset_password.Remove($_.Name) }
+
+  
+  #$json = $Password | convertto-json -Depth 5
+
+  write-host $(convertto-json $PasswordObject)
+  
+  $response = hudu_request -Method 'put' -Resource "/api/v1/asset_passwords/$id" -body $(convertto-json $PasswordObject)
+  
+  $response
+  
+  
+}
+
+function Remove-HuduPassword {
+  Param (
+    [Parameter(Mandatory=$true)][Int]$id =''
+  )
+    
+  $response = hudu_request -Method post -Resource "/api/v1/asset_passwords/$id"
+  
+  $response
+  
+  
 }
