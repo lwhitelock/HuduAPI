58ecf456c33ae4e4a88e117925e95224a8399f0a
lwhitelock <luke.whitelock@iqinit.uk>
2021-02-27 18:06:23 +0000

    Updated module manifest files and version

--- HuduAPI.psm1
+++ HuduAPI.psm1
@@ -1,10 +1,12 @@
-$Public = @( Get-ChildItem -Recurse -Path "$PSScriptRoot\Public\" -File -Filter *.ps1 )
-$Private = @( Get-ChildItem -Recurse -Path "$PSScriptRoot\Private\" -File -Filter *.ps1 )
-@($Public + $Private) | ForEach-Object {
-    Try {
-        If ($_.Length -gt 0) {. $_.FullName}
-    } Catch {
-        Write-Error -Message "Failed to import function $($_.FullName): $_"
+$Public  = @(Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue) + @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue)
+foreach ($import in @($Public)){
+    try
+    {
+        . $import.FullName
+    }
+    catch
+    {
+        Write-Error -Message "Failed to import function $($import.FullName): $_"
     }
 }
 Export-ModuleMember -Function $Public.BaseName -Alias *
