114fb11f97d22e7487f6b033a71be774d60acfa1
John Duprey <jwd@johnwduprey.com>
2023-02-26 14:00:10 -0500

    Cmdlet documentation, missing parameters
    Add missing parameters and cmdlet help to Remove functions

--- HuduAPI/Public/Remove-HuduArticle.ps1
+++ HuduAPI/Public/Remove-HuduArticle.ps1
@@ -1,12 +1,28 @@
 function Remove-HuduArticle {
-  [CmdletBinding()]
-  Param (
-    [Parameter(Mandatory = $true)]
-    [Int]$Id
-  )
-      
-  $Response = Invoke-HuduRequest -Method delete -Resource "/api/v1/articles/$Id"
-    
-  $Response
-    
+    <#
+    .SYNOPSIS
+    Delete a Knowledge Base Article
+
+    .DESCRIPTION
+    Uses Hudu API to remove a KB article
+
+    .PARAMETER Id
+    Id of the requested article
+
+    .EXAMPLE
+    Remove-HuduArticle -Id 1
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    Param (
+        [Parameter(ValueFromPipelineByPropertyName = $true, Mandatory = $true)]
+        [Int]$Id
+    )
+    process {
+        if ($PSCmdlet.ShouldProcess($Id)) {
+            Invoke-HuduRequest -Method delete -Resource "/api/v1/articles/$Id"
+        }
+    }
 }
--- HuduAPI/Public/Remove-HuduBaseURL.ps1
+++ HuduAPI/Public/Remove-HuduBaseURL.ps1
@@ -1,5 +1,18 @@
 function Remove-HuduBaseURL {
-	[CmdletBinding()]
-	Param()
-	Set-Variable -Name "Int_HuduBaseURL" -Value $null -Visibility Private -Scope script -Force
+    <#
+    .SYNOPSIS
+    Remove base URL
+
+    .DESCRIPTION
+    Unsets the Hudu Base URL variable
+
+    .EXAMPLE
+    Remove-HuduBaseURL
+
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    Param()
+    if ($PSCmdlet.ShouldProcess('Base URL')) {
+        Remove-Variable -Name 'Int_HuduBaseURL' -Scope script -Force
+    }
 }
