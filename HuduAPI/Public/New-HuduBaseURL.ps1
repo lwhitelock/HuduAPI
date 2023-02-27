7833f3b9664f2550110c4b5480971c0669a1bfee
John Duprey <jwd@johnwduprey.com>
2023-02-26 19:28:03 -0500

    PSScriptAnalyzer fixes

--- HuduAPI/Public/New-HuduBaseURL.ps1
+++ HuduAPI/Public/New-HuduBaseURL.ps1
@@ -23,16 +23,17 @@ function New-HuduBaseURL {
         [String]
         $BaseURL
     )
+    process {
+        if (!$BaseURL) {
+            $BaseURL = Read-Host -Prompt 'Please enter your Hudu Base URL with no trailing /, for example https://demo.huducloud.com :'
+        }
+        Set-Variable -Name 'Int_HuduBaseURL' -Value $BaseURL -Visibility Private -Scope script -Force
 
-    if (!$BaseURL) {
-        $BaseURL = Read-Host -Prompt 'Please enter your Hudu Base URL with no trailing /, for example https://demo.huducloud.com :'
-    }
-    Set-Variable -Name 'Int_HuduBaseURL' -Value $BaseURL -Visibility Private -Scope script -Force
-
-    if ($script:Int_HuduAPIKey) {
-        [version]$Version = (Get-HuduAppInfo).version
-        if ($Version -lt $script:HuduRequiredVersion) {
-            Write-Warning "A connection error occured or Hudu version is below $script:HuduRequiredVersion"
+        if ($script:Int_HuduAPIKey) {
+            [version]$Version = (Get-HuduAppInfo).version
+            if ($Version -lt $script:HuduRequiredVersion) {
+                Write-Warning "A connection error occured or Hudu version is below $script:HuduRequiredVersion"
+            }
         }
     }
 }
