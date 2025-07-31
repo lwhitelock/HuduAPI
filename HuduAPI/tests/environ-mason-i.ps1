2ea828cd97146fbebf15d80fc886dee169a69e87
masonville17 <anoniboni9876@gmail.com>
2025-07-31 17:31:31 +0000

    dear god, accidentally submitted env var. I dont think I'm working with new gitignore ;_;

--- HuduAPI/tests/environ-mason-i.ps1
+++
@@ -1,6 +0,0 @@
-$env:HUDU_API_KEY = "jPXEr8PQqw9W9FJYnx97XnVp"
-$env:HUDU_BASE_URL = "https://mason-i.hudu.app"
-$env:HUDU_TEST_RACK_ROLE_ID = 1
-$env:HUDU_TEST_COMPANY_ID = 1
-$env:HUDU_TEST_ASSET_ID = 1
-$env:HUDU_TEST_USER_ID = 1
