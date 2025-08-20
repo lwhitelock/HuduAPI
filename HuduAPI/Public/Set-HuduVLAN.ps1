8a5372ebd6f7ca992ec15ec6c77f5fad2321a3de
masonville17 <anoniboni9876@gmail.com>
2025-08-20 17:40:06 +0000

    got rid of set-archive for both. Just redundant. set for both is started

--- HuduAPI/Public/Set-HuduVLAN.ps1
+++ HuduAPI/Public/Set-HuduVLAN.ps1
@@ -2,11 +2,43 @@ function Set-HuduVLAN {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)] [int]$Id,
-        [string]$Name
+        [string]$Name,
+        [int]$CompanyId,
+        [string]$Description,
+        [int]$RoleListItemID,
+        [int]$StatusListItemID,
+        [int]$VLANId,
+        [int]$VLANZoneId,
+        [string]$Archived='false',
     )
+    $vlan = Get-HuduVLANs -id $Id
+    if ($Name) {
+        $vlan | Add-Member -MemberType NoteProperty -Name name -Force -Value $Name
+    }
+    if ($CompanyId) {
+        $vlan | Add-Member -MemberType NoteProperty -Name company_id -Force -Value $CompanyId
+    }
+    if ($Description) {
+        $vlan | Add-Member -MemberType NoteProperty -Name description -Force -Value $Description
+    }
+    if ($RoleListItemID) {
+        $vlan | Add-Member -MemberType NoteProperty -Name role_list_item_id -Force -Value $RoleListItemID
+    }
+    if ($StatusListItemID) {
+        $vlan | Add-Member -MemberType NoteProperty -Name status_list_item_id -Force -Value $StatusListItemID
+    }            
+    if ($VLANId) {
+        $vlan | Add-Member -MemberType NoteProperty -Name vlan_id -Force -Value $VLANId
+    }
+    if ($VLANZoneId) {
+        $vlan | Add-Member -MemberType NoteProperty -Name vlan_zone_id -Force -Value $VLANZoneId
+    }
+    if ($Archived) {
+        $vlan | Add-Member -MemberType NoteProperty -Name archived -Force -Value $Archived
+    }
 
     $payload = @{
-        name             = $Name
+        vlan             = $vlan
     } | ConvertTo-Json -Depth 10
 
     try {
--- HuduAPI/Public/Set-HuduVLANZone.ps1
+++ HuduAPI/Public/Set-HuduVLANZone.ps1
@@ -2,18 +2,39 @@ function Set-HuduVLANZone {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)] [int]$Id,
-        [string]$Name
+        [string]$Name,
+        [int]$CompanyId,
+        [string]$Description,
+        [int]$VLANIdRanges,
+        [string]$Archived
     )
+    $vlan_zone = Get-HuduVLANZones -id $Id
+    if ($Archived) {
+        $vlan_zone | Add-Member -MemberType NoteProperty -Name archived -Force -Value $Archived
+    }
+    if ($VLANIdRanges) {
+        $vlan_zone | Add-Member -MemberType NoteProperty -Name vlan_id_ranges -Force -Value $VLANIdRanges
+    }
+    if ($Description) {
+        $vlan_zone | Add-Member -MemberType NoteProperty -Name description -Force -Value $Description
+    }
+    if ($CompanyId) {
+        $vlan_zone | Add-Member -MemberType NoteProperty -Name company_id -Force -Value $CompanyId
+    }
+    if ($Name) {
+        $vlan_zone | Add-Member -MemberType NoteProperty -Name name -Force -Value $CompanyId
+    }            
+
 
     $payload = @{
-        name             = $Name
+        vlan_zone   = $vlan_zone
     } | ConvertTo-Json -Depth 10
 
     try {
         $res = Invoke-HuduRequest -Method PUT -Resource "/api/v1/vlan_zones/$Id" -Body $payload
         return $res.procedure
     } catch {
-        Write-Warning "Failed to update vlan zone ID $Id"
+        Write-Warning "Failed to archive vlan zone ID $Id"
         return $null
     }
 }
