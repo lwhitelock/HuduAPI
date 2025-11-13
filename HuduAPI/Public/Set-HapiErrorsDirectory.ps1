87bbd251205de818f32716c15ac58cccd9bb418d
masonville17 <anoniboni9876@gmail.com>
2025-11-13 12:59:25 -0500

    final small little tweak for defaults, made it return something useful

--- HuduAPI/Public/Set-HapiErrorsDirectory.ps1
+++ HuduAPI/Public/Set-HapiErrorsDirectory.ps1
@@ -1,9 +1,9 @@
 function Set-HapiErrorsDirectory {
     param(
         [Parameter()][string]$Path=$null,
-        [Parameter()][bool]$skipRetry=$false,
-        [Parameter()][ValidateSet("Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkMagenta","DarkYellow","Gray","DarkGray","Blue","Green","Cyan","Red","Magenta","Yellow","White")]
-        [string]$Color="DarkCyan")
+        [Parameter()][bool]$skipRetry=$null,
+        [Parameter()][ValidateSet("Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkMagenta","DarkYellow","Gray","DarkGray","Blue","Green","Cyan","Red","Magenta","Yellow","White",$null)]
+        [string]$Color=$null)
     if ([string]::IsNullOrWhiteSpace($Path)) {
         $Path = $script:HAPI_ERRORS_DIRECTORY ?? (Join-Path -Path $($env:LOCALAPPDATA) -ChildPath "$($("$(Get-HuduBaseURL)" -replace "https://",'') -replace "/",'')-errors")
     }
@@ -14,11 +14,16 @@ function Set-HapiErrorsDirectory {
     if ($null -ne $skipRetry) {
         $script:SKIP_HAPI_ERROR_RETRY = $skipRetry
     } else {
-        $script:SKIP_HAPI_ERROR_RETRY = $false
+        $script:SKIP_HAPI_ERROR_RETRY = $script:SKIP_HAPI_ERROR_RETRY ?? $false
     }
     if ($Color -and @("Black","DarkBlue","DarkGreen","DarkCyan","DarkRed","DarkMagenta","DarkYellow","Gray","DarkGray","Blue","Green","Cyan","Red","Magenta","Yellow","White") -contains $Color) {
         $script:HAPI_ERROR_COLOR = $Color
     } else {
-        $script:HAPI_ERROR_COLOR = $null
+        $script:HAPI_ERROR_COLOR = $script:HAPI_ERROR_COLOR  ?? "DarkCyan"
+    }
+    return [pscustomobject]@{
+        Path        = $script:HAPI_ERRORS_DIRECTORY
+        SkipRetry   = $script:SKIP_HAPI_ERROR_RETRY
+        Color       = $script:HAPI_ERROR_COLOR
     }
 }
